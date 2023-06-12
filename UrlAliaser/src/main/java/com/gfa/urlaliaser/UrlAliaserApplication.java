75604816b7a8fad7c2f7aa988ef3bbce1b5d945c
luca-trnka <lucie-trnkova76@seznam.cz>
2023-06-12 09:31:37 +0200

    new project created

---
+++ UrlAliaser/src/main/java/com/gfa/urlaliaser/UrlAliaserApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.urlaliaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlAliaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlAliaserApplication.class, args);
+	}
+
+}
---
+++ UrlAliaser/src/test/java/com/gfa/urlaliaser/UrlAliaserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfa.urlaliaser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlAliaserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
