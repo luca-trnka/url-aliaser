c840e482c81f5dd42773d74579b6b470d541c21e
luca-trnka <lucie-trnkova76@seznam.cz>
2023-06-12 11:08:34 +0200

    endpoint /a/alias done

--- UrlAliaser/src/main/java/com/gfa/urlaliaser/controllers/MainController.java
+++ UrlAliaser/src/main/java/com/gfa/urlaliaser/controllers/MainController.java
@@ -4,11 +4,15 @@ import com.gfa.urlaliaser.models.LinkEntry;
 import com.gfa.urlaliaser.repositories.LinkEntryRepository;
 import com.gfa.urlaliaser.services.LinkEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.net.URI;
 
 @Controller
 public class MainController {
@@ -57,6 +61,22 @@ public class MainController {
         linkEntry.setAlias("");
 
         return "main";
+    }
+
+    @GetMapping("/a/{alias}")
+    public ResponseEntity<?> getPageByAlias(@PathVariable String alias) {
+        LinkEntry linkEntry = linkEntryRepository.findByAlias(alias);
+
+        //increasing hitcount if the alias exists and set location for its url
+        if (linkEntry != null) {
+            linkEntry.incrementHitCount();
+            linkEntryRepository.save(linkEntry);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(URI.create(linkEntry.getUrl()));
+            return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+        } else {
+            return ResponseEntity.status(404).build();
+        }
     }
 
 
